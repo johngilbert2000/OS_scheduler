f78797c770f4c3281bdf75383a1d06285c1d18cf
John Gilbert <johngilbert2571@gmail.com>
2020-04-27 23:45:09 +0800

    small changes

--- definitions.h
+++ definitions.h
@@ -18,4 +18,11 @@ struct linked {
 
 typedef struct linked node;
 
+struct sched_param {
+    int sched_priority;
+};
+
+enum job_status {UNAVAILABLE, STARTED, FINISHED};
+typedef enum job_status jobstat;
+
 #endif
--- proc_step.c
+++ proc_step.c
@@ -8,15 +8,28 @@
 #include <fcntl.h> // for nonblocking file descriptor pipes
 #include "definitions.h"
 
-#define LOW_PRIORITY 10
-#define HIGH_PRIORITY 90
+#define LOW_PRIORITY 90
+#define HIGH_PRIORITY 10
 
 // TODO:
+// - Test swap_priorities
 // - sched_setscheduler / sched_yield
 // dmesg_info, timer
 
-enum job_status {UNAVAILABLE, STARTED, FINISHED};
-typedef enum job_status jobstat;
+// --------------
+// To do in main:
+
+// int pipe_ret;
+// jobstat fd[2];
+// // Ensure pipe file descriptor is set to nonblocking
+
+// fcntl(fd[0], F_SETFL, fcntl(fd[0], F_GETFL) | O_NONBLOCK);
+// fcntl(fd[1], F_SETFL, fcntl(fd[0], F_GETFL) | O_NONBLOCK);
+
+// // Create pipe
+// pipe_ret = pipe(fd);
+// if (pipe_ret == -1) { perror("pipe error"); exit(1); }
+// // --------------
 
 // struct job_data {
 //   pid PID;
@@ -34,91 +47,80 @@ void to_dmesg(pid PID, long long start_time, long long stop_time) {
     return; 
 }
 
+long long get_time(){
+    // gets the clock time in nanoseconds (from time.h)
+    struct timespec t;
+    clock_gettime(CLOCK_REALTIME, &t);
+    return (t.tv_sec*(int)1e9 + t.tv_nsec);
+}
+
 void time_unit(){
     volatile unsigned long i; for(i=0;i<1000000UL;i++); 
 }
-// --------------
-// To do in main:
-
-// int pipe_ret;
-// jobstat fd[2];
-// // Ensure pipe file descriptor is set to nonblocking
-
-// fcntl(fd[0], F_SETFL, fcntl(fd[0], F_GETFL) | O_NONBLOCK);
-// fcntl(fd[1], F_SETFL, fcntl(fd[0], F_GETFL) | O_NONBLOCK);
-
-// // Create pipe
-// pipe_ret = pipe(fd);
-// if (pipe_ret == -1) { perror("pipe error"); exit(1); }
-// // --------------
-
-struct sched_param {
-    int sched_priority;
-};
 
 void swap_priorities(pid oldPID, pid newPID) {
-   struct sched_param old_param, new_param;
-    // int old_priority, new_priority, main_priority;
-    int high_priority, low_priority;
-
-    // old_priority = getpriority(PRIO_PROCESS, oldPID);
-    // new_priority = getpriority(PRIO_PROCESS, newPID);
-    // main_priority = getpriority(PRIO_PROCESS, 0);
-    // new_priority = main_priority + 1;
+    // TODO: use nice() values instead
+    struct sched_param old_param, new_param;
 
     old_param.sched_priority = LOW_PRIORITY;
     new_param.sched_priority = HIGH_PRIORITY;
-
     sched_setscheduler(oldPID, SCHED_FIFO, &old_param);
     sched_setscheduler(newPID, SCHED_FIFO, &new_param);
 }
 
 void init_priority(pid PID) {
     struct sched_param param;
-    param.sched_priority = LOW_PRIORITY;
+    param.sched_priority = HIGH_PRIORITY;
     sched_setscheduler(PID, SCHED_FIFO, &param);
 }
 
-pid change_process(jobstat *stat, pid PID, pid prevPID, uint exec_time, jobstat pipefd[2], bool running) {
+pid start_process(jobstat *stat, uint exec_time, jobstat pipefd[2]) {
+    // Create new process with fork(); (used in process_control)
+    pid PID;
+    jobstat localstatus;
+    localstatus = *stat;
+
+    PID = fork();
+    if (PID == 0) { // CHILD PROCESS
+        // Pass start status into pipe
+        PID = getpid();
+        localstatus = STARTED;
+        write(pipefd[1], &localstatus, sizeof(localstatus));
+
+        // Run process
+        for (int i = 0; i < exec_time; i++) {
+            time_unit();
+        }
+
+        // Pass finished status into pipe and exit
+        localstatus = FINISHED;
+        write(pipefd[1], &localstatus, sizeof(localstatus));
+        exit(EXIT_SUCCESS);
+    }
+    else { // PARENT PROCESS
+        // Get start status from child process
+        read(pipefd[0], &localstatus, sizeof(localstatus));
+        *stat = localstatus;
+    }
+    return PID;
+}
+
+pid process_control(jobstat *stat, pid PID, pid prevPID, uint exec_time, jobstat pipefd[2], bool running) {
+    // Creates new process if stat indicates job has not started
+    // Otherwise, switches to process with given PID 
+    // from the previous process (prevPID)
+
     // stat: the status of the selected job
     // PID: PID of selected job, if it exists
     // prevPID: PID of previous job, if there was one
     // exec_time: execution time of selected job
     // pipefd: file descriptor for piping selected job status from child process
     // running: true if previous job is running, else false
-    jobstat localstatus;
-    localstatus = *stat;
-    
 
-    if (localstatus == UNAVAILABLE) {
-        // Create new process
-        PID = fork();
-        if (PID == 0) { // CHILD PROCESS
-            // Pass start status into pipe
-            PID = getpid();
-            localstatus = STARTED;
-            write(pipefd[1], &localstatus, sizeof(localstatus));
-
-            // Run process
-            for (int i = 0; i < exec_time; i++) {
-                time_unit();
-            }
-
-            // Pass finished status into pipe and exit
-            localstatus = FINISHED;
-            write(pipefd[1], &localstatus, sizeof(localstatus));
-            exit(EXIT_SUCCESS);
-        }
-        else { // PARENT PROCESS
-            // Get start status from child process
-            read(pipefd[0], &localstatus, sizeof(localstatus));
-            *stat = localstatus;
-        }
+    if (*stat == UNAVAILABLE) {
+        PID = start_process(stat, exec_time, pipefd);
     }
-    
-    // if (running == true) sched_yield(prevPID);
-    // sched_setscheduler(PID, SCHED_FIFO, &param);
-    if (running = true) {
+       if (running = true) {
         swap_priorities(prevPID, PID);
     }
     else {
@@ -127,6 +129,19 @@ pid change_process(jobstat *stat, pid PID, pid prevPID, uint exec_time, jobstat 
 
    return PID;
 }
+
+
+
+
+// originally part of swap_priorities
+     // old_priority = getpriority(PRIO_PROCESS, oldPID);
+    // new_priority = getpriority(PRIO_PROCESS, newPID);
+    // main_priority = getpriority(PRIO_PROCESS, 0);
+    // new_priority = main_priority + 1;
+
+// originally part of process_control
+    // if (running == true) sched_yield(prevPID);
+    // sched_setscheduler(PID, SCHED_FIFO, &param);
 
 // pid change_job(jobstat *stat) {
 //     if (*stat == UNAVAILABLE) {
